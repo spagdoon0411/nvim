2c0ab83314f5f0b42c9e2b5ea60298759b0275e7
spagdoon0411 <spandan.suthar@live.com>
2025-05-25 16:46:09 -0700

    add telescope binding for marks

--- init.lua
+++ init.lua
@@ -439,6 +439,8 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      vim.keymap.set('n', '<leader>sm', builtin.marks, { desc = '[S]each [M]arks' })
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
