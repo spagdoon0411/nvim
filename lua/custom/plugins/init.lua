3fd63a0014a75161923ee7751c94937e308f34d8
spagdoon0411 <spandan.suthar@live.com>
2025-05-26 09:08:52 -0700

    remove nvimtree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,36 +3,5 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'nvim-tree/nvim-tree.lua',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- optional for file icons
-    config = function()
-      require('nvim-tree').setup {
-        view = {
-          width = 50, -- Width of the file explorer window
-          side = 'left', -- Position: "left" or "right"
-          relativenumber = true, -- Display line numbers
-        },
-        renderer = {
-          icons = {
-            show = {
-              file = true,
-              folder = true,
-              folder_arrow = true,
-              git = true,
-            },
-          },
-        },
-        actions = {
-          open_file = {
-            quit_on_open = true, -- Close tree when opening a file
-          },
-        },
-      }
-      -- Keybinding for toggling nvim-tree
-      vim.keymap.set('n', '<leader>oft', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-    end,
-    chat_autocomplete = true, -- Enable autocompletion for CopilotChat
-  },
   { 'ellisonleao/glow.nvim', config = true, cmd = 'Glow' },
 }
