6bc11ebd99cca0d7a88c732b5264a025be614279
spagdoon0411 <spandan.suthar@live.com>
2025-05-26 09:06:38 -0700

    break theme into file

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,13 +4,6 @@
 -- See the kickstart.nvim README for more information
 return {
   {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    config = function()
-      vim.cmd 'colorscheme rose-pine'
-    end,
-  },
-  {
     'github/copilot.vim',
   },
   {
