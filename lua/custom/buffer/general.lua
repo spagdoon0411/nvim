f9b867fab21666aceb837d6597d264a2a926b5c8
spagdoon0411 <spandan.suthar@live.com>
2025-07-03 17:50:48 -0700

    add wrap toggle

--- lua/custom/buffer/general.lua
+++ lua/custom/buffer/general.lua
@@ -3,4 +3,5 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.wrap = false
 -- vim.opt.statuscolumn = '%s %l %r  '
--- lua/custom/keybindings/general.lua
+++ lua/custom/keybindings/general.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.keymap.set('n', '<leader>w', function()
+  vim.wo.wrap = not vim.wo.wrap
+end, { desc = 'Toggle line wrap' })
