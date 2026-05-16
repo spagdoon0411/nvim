a4004f101b6e0571f4930427abfb5104497328f5
spagdoon0411 <spandan.suthar@live.com>
2026-05-15 23:17:04 -0700

    Coalesce LSP tools

--- lua/custom/plugins/lsp-tools.lua
+++ lua/custom/plugins/lsp-tools.lua
@@ -1,4 +1,5 @@
 return {
+  -- Lua development: type hints and completions for vim APIs
   {
     'folke/lazydev.nvim',
     ft = 'lua',
@@ -8,23 +9,26 @@ return {
       },
     },
   },
-
   { 'Bilal2453/luvit-meta', lazy = true },
 
+  -- LSP and server configuration
   {
     'neovim/nvim-lspconfig',
     dependencies = {
       { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      { 'williamboman/mason-lspconfig.nvim' },
+      { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
       { 'j-hui/fidget.nvim', opts = {} },
-      'hrsh7th/cmp-nvim-lsp',
+      { 'hrsh7th/cmp-nvim-lsp' },
     },
     config = function()
       require('custom.lsp.attach').setup()
 
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      local capabilities = vim.tbl_deep_extend(
+        'force',
+        vim.lsp.protocol.make_client_capabilities(),
+        require('cmp_nvim_lsp').default_capabilities()
+      )
 
       local servers = require 'custom.lsp.servers'
       local ensure_installed = vim.tbl_keys(servers or {})
@@ -32,22 +36,179 @@ return {
       for server_name, server in pairs(servers) do
         server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
         vim.lsp.config(server_name, server)
-      end
-
-      require('mason').setup()
-
-      vim.list_extend(ensure_installed, { 'stylua' })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        automatic_enable = false,
-      }
-
-      for server_name in pairs(servers) do
         vim.lsp.enable(server_name)
       end
 
+      vim.list_extend(ensure_installed, { 'stylua' })
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      require('mason-lspconfig').setup { automatic_enable = false }
+
       require('custom.lsp.python').setup(capabilities)
+    end,
+  },
+
+  -- Completion
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      {
+        'L3MON4D3/LuaSnip',
+        build = (function()
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+      },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-path' },
+    },
+    config = function()
+      local cmp = require 'cmp'
+      local luasnip = require 'luasnip'
+
+      luasnip.config.setup {}
+
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        completion = { completeopt = 'menu,menuone,noinsert' },
+        window = {
+          completion = cmp.config.window.bordered {
+            winhighlight = 'Normal:Normal,FloatBorder:Comment,CursorLine:Visual,Search:None',
+          },
+          documentation = cmp.config.window.bordered {
+            winhighlight = 'Normal:Normal,FloatBorder:Comment,CursorLine:Visual,Search:None',
+          },
+        },
+        mapping = cmp.mapping.preset.insert {
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-Space>'] = cmp.mapping.complete {},
+          ['<C-l>'] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { 'i', 's' }),
+          ['<C-h>'] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { 'i', 's' }),
+        },
+        sources = {
+          { name = 'lazydev', group_index = 0 },
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'path' },
+        },
+      }
+    end,
+  },
+
+  -- Autopairs with nvim-cmp integration
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    dependencies = { { 'hrsh7th/nvim-cmp' } },
+    config = function()
+      require('nvim-autopairs').setup {}
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      require('cmp').event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
+
+  -- Debugging (DAP)
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      { 'rcarriga/nvim-dap-ui' },
+      { 'nvim-neotest/nvim-nio' },
+      { 'williamboman/mason.nvim' },
+      { 'jay-babu/mason-nvim-dap.nvim' },
+      { 'leoluz/nvim-dap-go' },
+    },
+    keys = function(_, keys)
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+      return {
+        { '<F5>', dap.continue, desc = 'Debug: Start/Continue' },
+        { '<F1>', dap.step_into, desc = 'Debug: Step Into' },
+        { '<F2>', dap.step_over, desc = 'Debug: Step Over' },
+        { '<F3>', dap.step_out, desc = 'Debug: Step Out' },
+        { '<leader>b', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
+        {
+          '<leader>B',
+          function()
+            dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+          end,
+          desc = 'Debug: Set Breakpoint',
+        },
+        { '<F7>', dapui.toggle, desc = 'Debug: See last session result.' },
+        unpack(keys),
+      }
+    end,
+    config = function()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+
+      require('mason-nvim-dap').setup {
+        automatic_installation = true,
+        handlers = {},
+        ensure_installed = { 'delve' },
+      }
+
+      dapui.setup {
+        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+        controls = {
+          icons = {
+            pause = '⏸',
+            play = '▶',
+            step_into = '⏎',
+            step_over = '⏭',
+            step_out = '⏮',
+            step_back = 'b',
+            run_last = '▶▶',
+            terminate = '⏹',
+            disconnect = '⏏',
+          },
+        },
+      }
+
+      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+      dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+      require('dap-go').setup {
+        delve = { detached = vim.fn.has 'win32' == 0 },
+      }
+    end,
+  },
+
+  -- Linting
+  {
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local lint = require 'lint'
+      lint.linters_by_ft = {
+        markdown = { 'markdownlint' },
+      }
+
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = vim.api.nvim_create_augroup('lint', { clear = true }),
+        callback = function()
+          lint.try_lint()
+        end,
+      })
     end,
   },
 }
