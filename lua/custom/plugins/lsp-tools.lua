03ee2d69248245af8dde562ef8b39097932df1f3
spagdoon0411 <spandan.suthar@live.com>
2026-05-15 23:22:17 -0700

    Move formatting configs into lsp-tools

--- lua/custom/plugins/lsp-tools.lua
+++ lua/custom/plugins/lsp-tools.lua
@@ -193,6 +193,49 @@ return {
     end,
   },
 
+  -- Formatting
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        local disable_filetypes = { c = false, cpp = false }
+        local lsp_format_opt = disable_filetypes[vim.bo[bufnr].filetype] and 'never' or 'fallback'
+        return {
+          timeout_ms = 500,
+          lsp_format = lsp_format_opt,
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        c = { 'clang-format' },
+        rust = { 'rustfmt' },
+        yaml = { 'prettier' },
+        svelte = { 'prettier' },
+        html = { 'prettier' },
+        css = { 'prettier' },
+        json = { 'prettierd', 'prettier' },
+        markdown = { 'prettier' },
+        javascript = { 'prettierd', 'eslint-lsp' },
+        typescript = { 'prettierd', 'eslint-lsp' },
+        cmake = { 'cmakelang' },
+        python = { 'black' },
+      },
+    },
+  },
+
   -- Linting
   {
     'mfussenegger/nvim-lint',
