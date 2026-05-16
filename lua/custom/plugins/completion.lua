e5e95d5b55e2d63e7acf7fcb43ad6a2f1e853e3e
spagdoon0411 <spandan.suthar@live.com>
2026-05-15 21:28:38 -0700

    Theme changes for completions

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -30,6 +30,14 @@ return {
           end,
         },
         completion = { completeopt = 'menu,menuone,noinsert' },
+        window = {
+          completion = cmp.config.window.bordered {
+            winhighlight = 'Normal:Normal,FloatBorder:Comment,CursorLine:Visual,Search:None',
+          },
+          documentation = cmp.config.window.bordered {
+            winhighlight = 'Normal:Normal,FloatBorder:Comment,CursorLine:Visual,Search:None',
+          },
+        },
         mapping = cmp.mapping.preset.insert {
           ['<C-n>'] = cmp.mapping.select_next_item(),
           ['<C-p>'] = cmp.mapping.select_prev_item(),
